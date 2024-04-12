f43153a08e1c24855838a23275bda92189de19ed
CarranzaXav <carranzax7@gmail.com>
2024-04-12 11:37:42 -0400

    Sizing update 1.6

--- netflix-ui/src/components/CardSlider.jsx
+++ netflix-ui/src/components/CardSlider.jsx
@@ -6,6 +6,7 @@ import styled from "styled-components";
 import { AiOutlineLeft, AiOutlineRight } from "react-icons/ai";
 import Card from "./Card";
 import "./Cardslider.css";
+import 'react-responsive-carousel/lib/styles/carousel.min.css'
 
 const CustomNextArrow = (props) => {
   const { onClick } = props;
